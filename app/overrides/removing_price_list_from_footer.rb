be67c56b840c44cf285223a72b369bc92454c180
bhanu <bhanu@bhanu.(none)>
2013-11-14 15:16:42 +0530

    bhanu

--- app/overrides/removing_price_list_from_footer.rb
+++ app/overrides/removing_price_list_from_footer.rb
@@ -3,5 +3,9 @@ Deface::Override.new(:virtual_path => "spree/shared/_filters",
 :name => "removing filters",
 :remove => "[data-hook='navigation'],erb[loud]:contains('t(:search)')"
 )
+Deface::Override.new(:virtual_path => "spree/shared/_sidebar",
+:name => "removing sidebar",
+:remove => "[id='sidebar']"
+)
 
 ############################################## END OF WORKING CODE ##############################################
